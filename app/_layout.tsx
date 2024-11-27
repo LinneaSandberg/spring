8b9ebed42a65063fd0693522503713767e1c421b
Linnea Sandberg <llinnea.sandberg@gmail.com>
2024-11-28 11:41:13 +1300

    Support for dark and lightmode in settings.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,39 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import AuthContextProvider from '../context/AuthContextProvider';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    Uncut: require('../assets/fonts/UncutSans-Variable.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+
   return (
     <AuthContextProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="login/index" />
-        <Stack.Screen name="signup/index" />
-        <Stack.Screen name="home/index" />
-      </Stack>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="login/index" />
+          <Stack.Screen name="signup/index" />
+          <Stack.Screen name="home/index" />
+        </Stack>
+      </ThemeProvider>
     </AuthContextProvider>
   );
 }
